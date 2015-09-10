5a53c3b3fd01964c4ed10b36803b688fc93817a8
zipper97412 <zipper97412@gmail.com>
2015-09-10 14:32:01 +0200

    Create coreLoop.c

---
+++ coreLoop.c
@@ -0,0 +1,36 @@
+
+#include "stm8s.h"
+
+void extracteur(char phrase[], char words[3][20]) 
+{	
+	char j=0, k, l, record=0;
+	char used[3]={0,0,0}; 
+	
+	while (phrase[j] != '\0') 
+	{
+		if (phrase[j] == '/') 
+		{
+			record=1;
+			switch (phrase[j+1])
+			{
+				case '1':k=0;used[0]=1;break;
+				case '2':k=1;used[1]=1;break;
+				case '3':k=2;used[2]=1;break;
+			}
+			l=0;
+			j+=2;
+		}
+		if (record == 1)
+		{
+			words[k][l]=phrase[j];
+			l++;
+			
+			if ((phrase[j+1] == ' ') || (phrase[j+1] == '\0'))
+			{
+				words[k][l]='\0';
+				record=0;
+			}
+		}
+		j++;
+	}
+}
