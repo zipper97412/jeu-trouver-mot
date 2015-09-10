cf1756ceaf8376156a7e1355049b6940a8f89a4c
zipper97412 <zipper97412@gmail.com>
2015-09-10 14:37:32 +0200

    Update coreLoop.c

--- coreLoop.c
+++ coreLoop.c
@@ -1,10 +1,11 @@
 
 #include "stm8s.h"
+#define NB_THEME 4
 
-void extracteur(char phrase[], char words[3][20]) 
+void parseur(char phrase[], char words[NB_THEME][20]) 
 {	
 	char j=0, k, l, record=0;
-	char used[3]={0,0,0}; 
+	char used[NB_THEME]={0,0,0,0}; 
 	
 	while (phrase[j] != '\0') 
 	{
@@ -16,6 +17,7 @@ void extracteur(char phrase[], char words[3][20])
 				case '1':k=0;used[0]=1;break;
 				case '2':k=1;used[1]=1;break;
 				case '3':k=2;used[2]=1;break;
+				case '4':k=3;used[3]=1;break;
 			}
 			l=0;
 			j+=2;
