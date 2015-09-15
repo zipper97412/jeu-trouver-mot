61ee8050d55967c8ea5daa3929328bd26fceb2e3
zipper97412 <zipper97412@gmail.com>
2015-09-15 11:47:56 +0200

    Update datastruct.h

--- datastruct.h
+++ datastruct.h
@@ -1,12 +1,10 @@
 #ifndef DATASTRUCT
 #define DATASTRUCT
-#define NB_CHAR_NOM_THEME 30
-#include "stm8s_type.h"
+
 
 typedef struct {
   char actif;
   char idPlayer;
-  char nom[NB_CHAR_NOM_THEME];
   
 } THEME;
 
@@ -18,6 +16,10 @@ typedef struct {
 #define TRY_LOST 2
 #define TRY_WON 1
 #define TRY_NOPE 0
+
+#define FOUND_NOT 0
+#define FOUND_YES 1
+
 typedef struct {
   char idTheme;
   char actif;
